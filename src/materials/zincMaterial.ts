32657ba73cc13c39a3243a766adca3b2f12bdb14
John Rees <john@bitsushi.com>
2020-05-05 10:49:57 +0100

    update materials

--- src/materials/zincMaterial.ts
+++ src/materials/zincMaterial.ts
@@ -2,39 +2,30 @@ import * as THREE from "three";
 
 import { clipPlanes } from "../components/ClippingSlider";
 
-export const fill = new THREE.MeshLambertMaterial({
-  color: "white",
-  // flatShading: true,
-  clippingPlanes: clipPlanes,
-  clipIntersection: true,
-  clipShadows: true
-  // side: THREE.DoubleSide,
-  // shadowSide: THREE.DoubleSide
-});
-
 const tl = new THREE.TextureLoader();
 
 const rpt = function(texture: THREE.Texture) {
   texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
   texture.repeat.set(0.025, 0.025);
+  texture.rotation = Math.PI / 2;
 };
 
-const plywoodMaterial = new THREE.MeshStandardMaterial({
+const zincMaterial = new THREE.MeshStandardMaterial({
   color: 0xeeeeee,
   map: tl.load(
-    "materials/46_plywood texture-seamless_hr/46_plywood texture-seamless_hr_DIFFUSE.jpg",
+    "materials/16_steel zinc coated corrugated metal texture-seamless_hr/16_steel zinc coated_DIFF.jpg",
     rpt
   ),
   displacementMap: tl.load(
-    "materials/46_plywood texture-seamless_hr/46_plywood texture-seamless_hr_DISPL.jpg",
+    "materials/16_steel zinc coated corrugated metal texture-seamless_hr/16_steel zinc coated_DISPL.jpg",
     rpt
   ),
   normalMap: tl.load(
-    "materials/46_plywood texture-seamless_hr/46_plywood texture-seamless_hr_NORM.jpg",
+    "materials/16_steel zinc coated corrugated metal texture-seamless_hr/16_steel zinc coated_NORM.jpg",
     rpt
   ),
   aoMap: tl.load(
-    "materials/46_plywood texture-seamless_hr/46_plywood texture-seamless_hr-AO.jpg",
+    "materials/16_steel zinc coated corrugated metal texture-seamless_hr/16_steel zinc coated-AO.jpg",
     rpt
   ),
   // side: THREE.DoubleSide
@@ -43,12 +34,13 @@ const plywoodMaterial = new THREE.MeshStandardMaterial({
   // normalScale: 1.0,
 
   // ambientIntensity: 0.3,
-  aoMapIntensity: 3.0,
-  envMapIntensity: 1.5,
+  // aoMapIntensity: 3.0,
+  // envMapIntensity: 10.5,
   // https://discourse.threejs.org/t/material-displacement-map-makes-the-texture-unwrap-the-models-surfaces/5119/11
   displacementScale: 0,
+  // displacementBias: 1,
   roughness: 0.8,
-  metalness: 0,
+  metalness: 0.4,
   // side: THREE.DoubleSide,
   polygonOffset: true,
   polygonOffsetFactor: 1,
@@ -57,8 +49,9 @@ const plywoodMaterial = new THREE.MeshStandardMaterial({
   clippingPlanes: clipPlanes,
   clipIntersection: true,
   // shadowSide: THREE.DoubleSide,
-  side: THREE.FrontSide
+  side: THREE.FrontSide,
+  clipShadows: true
   // clipShadows: true
 });
 
-export default plywoodMaterial;
+export default zincMaterial;
